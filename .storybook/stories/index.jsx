eedc5dc440985912937189b0e2f796b0faf33ef3
Dávid Csejtei <csejteidavid90@gmail.com>
2019-07-16 21:11:52 +0200

    Fixed wrong file extension and function call

--- .storybook/stories/index.jsx
+++ .storybook/stories/index.jsx
@@ -3,4 +3,4 @@ import { storiesOf, setAddon } from '@storybook/react';
 
 import CustomButton from '../../src/index';
 
-storiesOf("Custom button", module).addWithJSX("simple", () => <CustomButton />);
+storiesOf("Custom button", module).add("simple", () => <CustomButton />);
